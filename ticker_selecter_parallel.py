ac352b6d3998d2c02aeb63bfcf3170ea7da7739a
Ricky Super <xyzstartdoingthings@gmail.com>
2024-03-11 15:31:09 -0500

    optimizer

---
+++ ticker_selecter_parallel.py
@@ -0,0 +1,25 @@
+from concurrent.futures import ProcessPoolExecutor
+from utils import *
+from algo_2 import *
+
+data_path = Path(os.getcwd()) # perhaps set to external harddrive to accomodate large amount of data
+path = data_path / 'stock price data'
+ticker_all = pd.read_csv(path/"finer_data_complete_stock.csv")
+a=24
+variables={"atr_len":np.arange(11,14,1), "macd_fastLen": np.arange(11,14,1), "macd_slowLen":np.arange(32,36,2), "macd_signalSmooth":np.arange(7,10,1), "macd_peakLen":[3,5], "gain_ratio":np.arange(1,3,1), "loss_ratio": np.arange(1,3,1), "peak2_len":[20,30], "peak3_len":[40,50]}
+
+def optimizer_parallel(algo, ticker, variables):
+    return optimizer(algo, ticker, variables)[0]
+
+def main():
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(optimizer_parallel, algo2, ticker, variables) for ticker in ticker_all["Symbol"].unique()[0:96]]
+        top_a_tickers = [future.result() for future in futures]
+
+    # Sort and select the top 'a' results
+    top_a_tickers.sort(key=lambda x: x[1], reverse=True)
+    top_a_tickers = top_a_tickers[:a]
+    return top_a_tickers
+
+if __name__ == '__main__':
+    print(main())

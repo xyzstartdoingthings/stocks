40c15a7954fabe36f5f2ae99b8d5b0e0ca808c94
xyzstartdoingthings <xyzstartdoingthings@gmail.com>
2024-03-17 02:20:34 -0500

    finer

--- ticker_selecter_parallel.py
+++ ticker_selecter_parallel.py
@@ -3,16 +3,22 @@ from utils import *
 from algo_2 import *
 import pickle
 
-# perhaps set to external harddrive to accomodate large amount of data
-data_path = Path(os.getcwd())
-path = data_path / 'stock price data'
-ticker_all = pd.read_csv(path/"finer_data_complete_stock.csv")
-# change
-ticker_range = range(485, 677)
-a = int(len(ticker_range)/6)
-variables = {"atr_len": np.arange(11, 14, 1), "macd_fastLen": np.arange(11, 14, 1), "macd_slowLen": np.arange(32, 36, 2), "macd_signalSmooth": np.arange(
-    7, 10, 1), "macd_peakLen": [3, 5], "gain_ratio": np.arange(1, 3, 1), "loss_ratio": np.arange(1, 3, 1), "peak2_len": [20,30], "peak3_len": [40,50]}
+# # coarse selecting
+# data_path = Path(os.getcwd())
+# path = data_path / 'stock price data'
+# ticker_all = pd.read_csv(path/"finer_data_complete_stock.csv")
+# # change
+# ticker_range = range(485, 677)
+# a = int(len(ticker_range)/6)
 
+#finer selecting
+data_path = Path(os.getcwd())
+with open('ticker_coarse','rb') as f:
+     ticker_all = pickle.load(f)
+ticker_range = range(168, len(ticker_all))
+a = int(len(ticker_range))
+variables = {"atr_len": np.arange(10, 13, 1), "macd_fastLen": np.arange(10, 14, 1), "macd_slowLen": np.arange(30, 36, 2), "macd_signalSmooth": np.arange(
+    6, 9, 1), "macd_peakLen": [3], "gain_ratio": np.arange(1, 2.5, 0.5), "loss_ratio": np.arange(1.5, 3, 0.5), "peak2_len": [20], "peak3_len": [40,50]}
 
 def optimizer_parallel(algo, ticker, variables):
     result = optimizer(algo, ticker, variables, max_dd=0.15, wl_ratio=0.7)
@@ -34,7 +40,7 @@ def main():
     top_a_tickers = [ele for ele in top_a_tickers if ele != []]
     top_a_tickers.sort(key=lambda x: x[0][1], reverse=True)
     top_a_tickers = top_a_tickers[:a]
-    with open("output "+str(ticker_range[0])+" to "+str(ticker_range[-1]), "wb") as fp:   #Pickling
+    with open("output finer "+str(ticker_range[0])+" to "+str(ticker_range[-1]), "wb") as fp:   #Pickling
         pickle.dump(top_a_tickers, fp)
 
     return top_a_tickers

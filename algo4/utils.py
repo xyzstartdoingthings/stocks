5c9e2048a969fe569e166a8da757395ef7b83ce1
xyzstartdoingthings <xyzstartdoingthings@gmail.com>
2024-03-19 20:32:36 -0500

    optimize

--- algo4/utils.py
+++ algo4/utils.py
@@ -9,7 +9,7 @@ import itertools
 import pickle
 
 
-def optimizer(algo, ticker, variables, n=3, min_ROI = 0.3, max_dd=0.18, wl_ratio=0.6, eval=eval):
+def optimizer(algo, ticker, variables, n=3, min_ROI = 1, max_dd=0.18, wl_ratio=0.6, eval=eval):
     variables["ticker"] = [ticker]
     keys, values = zip(*variables.items())
     combinations = itertools.product(*values)
